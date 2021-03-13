678efac4b63d19f84d2236da262060225affea88
Betelgeuse1 <avanalos@gmail.com>
2021-03-13 17:55:22 +0100

    :pencil2: A typo in the port reading

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.get('/random', (_, res, next) => {
 
 // Launch the server once the connection to the db is opened
 mongoose.connection.once('open', function() {
-    app.listen(process.end.PORT || 3030, () => console.log('Server is running'))
+    app.listen(process.env.PORT || 3030, () => console.log('Server is running'))
 });
